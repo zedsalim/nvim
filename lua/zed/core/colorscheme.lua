4c3fd3c60ee2851f1d94254fb353092cb1ab0805
Salim Zaidi <121177411+zedsalim@users.noreply.github.com>
2023-06-28 14:33:29 +0100

    Update colorscheme.lua

--- lua/zed/core/colorscheme.lua
+++ lua/zed/core/colorscheme.lua
@@ -1,6 +1,6 @@
 -- set colorscheme to nightfly with protected call
 -- in case it isn't installed
-local status, _ = pcall(vim.cmd, "colorscheme tokyonight-storm")
+local status, _ = pcall(vim.cmd, "colorscheme onedark")
 if not status then
 	print("Colorscheme not found!") -- print error if colorscheme not installed
 	return
