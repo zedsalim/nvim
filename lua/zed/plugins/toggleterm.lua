967ef07df618ae0c93322e885364dc1e94db9c2c
Salim Zaidi <121177411+zedsalim@users.noreply.github.com>
2023-07-09 13:29:48 +0100

    Update toggleterm.lua

--- lua/zed/plugins/toggleterm.lua
+++ lua/zed/plugins/toggleterm.lua
@@ -27,12 +27,12 @@ toggleterm.setup({
 })
 
 function _G.set_terminal_keymaps()
-  local opts = {noremap = true}
-  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<Left>', [[<C-\><C-n><C-W>h]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<Down>', [[<C-\><C-n><C-W>j]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<Up>', [[<C-\><C-n><C-W>k]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<Right>', [[<C-\><C-n><C-W>l]], opts)
+	local opts = { noremap = true }
+	--   vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+	--   vim.api.nvim_buf_set_keymap(0, 't', '<Left>', [[<C-\><C-n><C-W>h]], opts)
+	--   vim.api.nvim_buf_set_keymap(0, 't', '<Down>', [[<C-\><C-n><C-W>j]], opts)
+	--   vim.api.nvim_buf_set_keymap(0, 't', '<Up>', [[<C-\><C-n><C-W>k]], opts)
+	--   vim.api.nvim_buf_set_keymap(0, 't', '<Right>', [[<C-\><C-n><C-W>l]], opts)
 end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
