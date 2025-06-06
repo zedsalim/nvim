9962b33605004bb4d4c2a299538998b3221364af
zedsalim <salimzaiidi@gmail.com>
2025-06-06 13:49:23 +0100

    chore: update header ASCII art

--- lua/zed/plugins/alpha.lua
+++ lua/zed/plugins/alpha.lua
@@ -7,23 +7,23 @@ return {
 
     -- Set header
     dashboard.section.header.val = {
-      "                                                     ",
-      "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-      "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-      "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-      "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-      "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-      "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-      "                                                     ",
+      "                         ",
+      "███████╗███████╗██████╗ ",
+      "╚══███╔╝██╔════╝██╔══██╗",
+      "  ███╔╝ █████╗  ██║  ██║",
+      " ███╔╝  ██╔══╝  ██║  ██║",
+      "███████╗███████╗██████╔╝",
+      "╚══════╝╚══════╝╚═════╝ ",
+      "                         ",
     }
 
     -- Set menu
     dashboard.section.buttons.val = {
       dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-      dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-      dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
-      dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-      dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+      dashboard.button("SPACE n", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+      dashboard.button("CTRL p", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
+      dashboard.button("SPACE fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
+      dashboard.button("SPACE wr", "󰁯  > Restore Session For Current Dir", "<cmd>SessionRestore<CR>"),
       dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
     }
 
