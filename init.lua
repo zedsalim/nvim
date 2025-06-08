d0abeb9020ad7afab82fbf66fa70028bd8c7d317
zedsalim <salimzaiidi@gmail.com>
2025-06-08 07:34:59 +0100

    feat(nvim): add commands to toggle Arabic bidi support

--- init.lua
+++ init.lua
@@ -3,3 +3,6 @@ vim.env.PATH = vim.env.PATH .. ":/home/zed/.nvm/versions/node/v22.14.0/bin"
 require("zed.core")
 require("zed.lazy")
 require("colorizer").setup()
+
+vim.api.nvim_create_user_command("Ara", "set termbidi", {})
+vim.api.nvim_create_user_command("Nara", "set termbidi!", {})
